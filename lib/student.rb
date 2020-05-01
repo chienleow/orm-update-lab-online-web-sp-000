1aa198f001d625ae9c15f2533ed3fb34506f1a98
C. Leow <chienleow1@gmail.com>
2020-05-01 04:12:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,12 +2,12 @@ require_relative "../config/environment.rb"
 
 class Student
 
-  attr_accessor :name, :grade, :id
+  attr_accessor :id, :name, :grade
 
-  def initialize(name, grade, id = nil)
+  def initialize(id = nil, name, grade)
+    @id = id
     @name = name
     @grade = grade
-    @id = id
   end
 
   def self.create_table
