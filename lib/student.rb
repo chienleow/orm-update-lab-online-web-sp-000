e57f1a87a18aab2f4a1304de946deac4521482da
C. Leow <chienleow1@gmail.com>
2020-05-01 04:03:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,7 +57,7 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      WHEN name = ?
+      WHERE name = ?
       LIMIT 1
     SQL
     DB[:conn].execute(sql, name).map do |row|
