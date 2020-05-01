02f0a8b5888e5cae52ab7838a3c67d94577e6fc0
C. Leow <chienleow1@gmail.com>
2020-05-01 04:19:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,13 +45,6 @@ class Student
     student
   end
 
-  # def self.new_from_db(row)
-  #     id = row[0]
-  #     name = row[1]
-  #     grade = row[2]
-  #     self.new(id, name, grade)
-  #   end
-
   def self.new_from_db(row)
     new_student = self.new(id, name, grade)
     new_student.id = row[0]
