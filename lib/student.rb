583eaf9f140bf8563d94d26f8eab6a758bf5bd10
C. Leow <chienleow1@gmail.com>
2020-05-01 03:33:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,6 +13,23 @@ class Student
   def self.create_table
   end
 
+  def self.drop_table
+  end
+
+  def save
+  end
+
+  def self.create
+  end
+
+  def self.new_from_db
+  end
+
+  def self.find_by_name
+  end
+
+  def update
+  end
 
 
 
